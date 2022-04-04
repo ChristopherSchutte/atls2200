37784eb4d9d847f2ddedb1460c444a7494ac78de
Christopher Schutte <chsc2732@colorado.edu>
2022-04-03 22:39:42 -0600

    added JS assignment

--- JS/functions.js
+++ JS/functions.js
@@ -3,3 +3,49 @@ function menuOnClick() {
     document.getElementById("nav").classList.toggle("change");
     document.getElementById("menu-bg").classList.toggle("change-bg");
   }
+
+var id = null;
+function myMove() {
+  var elem = document.getElementById("jsAnimation");   
+  var pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 10);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++; 
+      elem.style.top = pos + 'px'; 
+      elem.style.left = pos + 'px'; 
+    }
+  }
+}
+
+const text = document.querySelector(".fancy");
+const strText = text.textContent;
+const splitText = strText.split("");
+
+text.textContent = "";
+
+for(let i=0; i < splitText.length; i++){
+  text.innerHTML += "<span>"+ splitText[i] + "</span>"
+}
+
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick(){
+  const span = text.querySelectorAll('span')[char];
+  span.classList.add('fade');
+  char++;
+  if(char === splitText.length){
+    complete();
+    return;
+  }
+}
+
+function complete(){
+  clearInterval(timer);
+  timer = null;
+}
+
